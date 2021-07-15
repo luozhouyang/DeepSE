5b0bdd1fc199a9ae7fe118119d3b5dd228837e4e
luozhouyang <zhouyang.luo@gmail.com>
2021-07-15 11:06:19 +0800

    Refactoring simcse training script

--- run_simcse.py
+++ run_simcse.py
@@ -5,14 +5,17 @@ import tensorflow as tf
 from tokenizers import BertWordPieceTokenizer
 
 from deepse.simcse import SimCSE
-from deepse.simcse_dataset import SimCSEDataset
+from deepse.simcse_dataset import (HardNegativeSimCSEDataset,
+                                   SupervisedSimCSEDataset, UnsupSimCSEDataset)
 
 CONFIG = {
     'pretrained_model_dir': os.environ['PRETRAINED_MODEL_PATH'],
     'model_dir': 'models/simcse',
     'epochs': 10,
     'lr': 3e-5,
-    'pooling_strategy': 'cls'
+    'pooling_strategy': 'cls',  # ['cls', 'pooler']
+    'mode': 'unsup',  # ['unsup', 'sup', 'hardneg']
+    'max_sequence_length': 256
 }
 
 CONFIG.update({
@@ -36,18 +39,26 @@ model = SimCSE(
 )
 
 tokenizer = BertWordPieceTokenizer.from_file(os.path.join(CONFIG['pretrained_model_dir'], 'vocab.txt'))
-dataset = SimCSEDataset(tokenizer=tokenizer)
+if CONFIG['mode'] == 'sup':
+    dataset = SupervisedSimCSEDataset(tokenizer=tokenizer)
+elif CONFIG['mode'] == 'hardneg':
+    dataset = HardNegativeSimCSEDataset(tokenizer=tokenizer)
+else:
+    dataset = UnsupSimCSEDataset(tokenizer=tokenizer)
+
 train_dataset = dataset(
     input_files=CONFIG['train_input_files'],
     batch_size=CONFIG['train_batch_size'],
     bucket_boundaries=CONFIG['train_bucket_boundaries'],
     buffer_size=CONFIG['train_buffer_size'],
+    max_sequence_length=CONFIG['max_sequence_length'],
 )
 valid_dataset = dataset(
     input_files=CONFIG['valid_input_files'],
     batch_size=CONFIG['valid_batch_size'],
     bucket_boundaries=CONFIG['valid_bucket_boundaries'],
     buffer_size=CONFIG['valid_buffer_size'],
+    max_sequence_length=CONFIG['max_sequence_length'],
 )
 
 # 开始训练模型
@@ -62,7 +73,7 @@ model.fit(
             save_weights_only=True),
         # 保存SavedModel格式的模型，用于Serving部署
         tf.keras.callbacks.ModelCheckpoint(
-            filepath=os.path.join(CONFIG['model_dir'], 'export/{epoch}'), 
+            filepath=os.path.join(CONFIG['model_dir'], 'export/{epoch}'),
             save_weights_only=False)
         # TODO: 增加性能评估的回调
     ]
