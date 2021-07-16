242ea4fa319185ce53b4d66737226fdea07df143
luozhouyang <zhouyang.luo@gmail.com>
2021-07-16 10:59:58 +0800

    Release v0.0.4

--- deepse/__init__.py
+++ deepse/__init__.py
@@ -10,4 +10,4 @@ from .simcse import (HardNegativeSimCSEModel, SimCSE, SupervisedSimCSEModel,
 logging.basicConfig(format='%(asctime)s %(levelname)s %(filename)7s %(lineno)4d] %(message)s', level=logging.INFO)
 
 __name__ = "deepse"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt', mode='rt', encoding='utf-8') as fin:
 
 setuptools.setup(
     name="deepse",
-    version="0.0.3",
+    version="0.0.4",
     description="**DeepSE**: **Sentence Embeddings** based on Deep Nerual Networks, designed for **PRODUCTION** enviroment!",
     long_description=long_description,
     long_description_content_type="text/markdown",
