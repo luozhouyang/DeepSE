5b7cbd2923a51e9baa0c3d08bcd4a2b8a3f5b944
luozhouyang <zhouyang.luo@gmail.com>
2021-11-22 19:25:52 +0800

    Remove py35

--- setup.py
+++ setup.py
@@ -29,10 +29,10 @@ setuptools.setup(
     license="Apache Software License",
     classifiers=(
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
