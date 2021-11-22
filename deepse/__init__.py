f31168644c73836a0035056efe6e040eb991b6e6
luozhouyang <zhouyang.luo@gmail.com>
2021-11-22 19:14:23 +0800

    Remove files

--- deepse/__init__.py
+++ deepse/__init__.py
@@ -1,13 +1,6 @@
 import logging
 
-from .bert_embedding import (BertAllInOneEmbeddingModel, BertAvgEmbeddingModel,
-                             BertCLSEmbeddingModel,
-                             BertFirstLastAvgEmbeddingModel,
-                             BertPoolerEmbeddingModel)
-from .simcse import (HardNegativeSimCSEModel, SimCSE, SupervisedSimCSEModel,
-                     UnsupSimCSEModel)
-
-logging.basicConfig(format='%(asctime)s %(levelname)s %(filename)7s %(lineno)4d] %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)7s %(lineno)4d] %(message)s", level=logging.INFO)
 
 __name__ = "deepse"
 __version__ = "0.0.4"
